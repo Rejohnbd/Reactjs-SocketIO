f8e60ddb226094a13c2de7a959430633b291d3a8
Rejohn <31244614+Rejohnbd@users.noreply.github.com>
2021-10-25 19:20:09 +0600

    Implement Body-Parser

--- atdn-system-back/index.js
+++ atdn-system-back/index.js
@@ -4,6 +4,7 @@ const io = require("socket.io")(http);
 
 const express = require("express");
 
+app.use(bodyParser.json({ limit: "50mb" }));
 app.use(express.json());
 
 app.post("/", (req, res) => {
